822c30d6d75f748b5e64f8268503c5d8ea8f43f2
Sachin Mahajan <sachinm78@gmail.com>
2019-02-28 17:54:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(array)
         nuarray.push ("#{counter}. #{name}")
         counter += 1 
       end
-      puts "The line is currently: #nuarray.join (" ")}"
+      puts "The line is currently: #{nuarray.join (" ")}"
     else
       puts "The line is currently empty"
     end
