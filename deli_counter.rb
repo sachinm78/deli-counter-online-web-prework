487ba04d6c4aa6a1ce43dbb3efe21d517b07ef24
Sachin Mahajan <sachinm78@gmail.com>
2019-02-28 17:56:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,11 +16,10 @@ end
 
 
 def take_a_number(line, new_person)
-  line.push(new_person) # could say: "line << new_person"
-  puts "Welcome, #{new_person}. You are number #{line.length} in line."
+    
 end
 
-take_a_number(katz_deli, "Fyvish")
+
 
 def now_serving(line)
   if line.length == 0 # could say: "if deli.empty?"
@@ -30,7 +29,3 @@ def now_serving(line)
     line.shift # this works in the IDE but no on repl.it
   end
 end
-
-puts now_serving(katz_deli)
-puts katz_deli
-
